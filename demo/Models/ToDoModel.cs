bda4746d51daab05c5e120a7b3b7dd7b9bca8eb7
Tri Bui Quang <bqdtri@tma.com.vn>
2020-03-25 16:42:06 +0700

    Update model and some caculate to todo child viewModel

---
+++ demo/Models/ToDoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.Models
+{
+    class ToDoModel
+    {
+        public int Id { get; set; }
+        public string Describtion { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
