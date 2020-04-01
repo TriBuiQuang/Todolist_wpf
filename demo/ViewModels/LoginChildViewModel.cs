fbd104e3d4877e17bd9069d632d69d1b3ee1211d
Hoàng Trần <minhhoang1601@gmail.com>
2020-04-01 12:03:47 +0700

    Login Page

--- demo/ViewModels/LoginChildViewModel.cs
+++ demo/ViewModels/LoginChildViewModel.cs
@@ -4,10 +4,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace demo.ViewModels
 {
     class LoginChildViewModel : Screen
     {
-    }
+		private string _userName;
+		private string _password;
+		public string Username
+		{
+			get { return _userName; }
+			set 
+			{
+				_userName = value;
+				NotifyOfPropertyChange(() => Username);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public string Password
+		{
+			get { return _password; }
+			set {
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public bool CanLogIn
+		{
+			get
+			{
+				bool output = false;
+				if (Username?.Length > 0 && Password?.Length > 0)
+				{
+					output = true;
+				}
+				return output;
+			}	
+		}
+
+		public void LogIn()
+		{
+			if (Username == "Hoang" && Password == "123")
+			{
+				MessageBox.Show("Log in success");
+			} else
+			{
+				MessageBox.Show("Log in fail");
+			}
+				
+		}
+	}
 }
